b0980601170b672b5820f2fce472e50f7f16581f
Your Name <you@example.com>
2020-08-28 22:49:40 +0300

    Updates

---
+++ precache-manifest.17794695c9eba1e40dadf791a9548f10.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1384d6f2985059997ea46a96f42a9cf",
+    "url": "/new-/index.html"
+  },
+  {
+    "revision": "54cdf03b6b0588ef2f6b",
+    "url": "/new-/static/css/2.9f2ac16c.chunk.css"
+  },
+  {
+    "revision": "77ffba9db59c23bac14b",
+    "url": "/new-/static/css/main.0725e599.chunk.css"
+  },
+  {
+    "revision": "54cdf03b6b0588ef2f6b",
+    "url": "/new-/static/js/2.fb77b208.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/new-/static/js/2.fb77b208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ffba9db59c23bac14b",
+    "url": "/new-/static/js/main.465d3419.chunk.js"
+  },
+  {
+    "revision": "9563b391950c947cfcf5",
+    "url": "/new-/static/js/runtime-main.5d65d2d0.js"
+  },
+  {
+    "revision": "24b63778d30181572e89dc2ec2d76998",
+    "url": "/new-/static/media/Cosmetics.24b63778.png"
+  },
+  {
+    "revision": "768dd837aba4d17ef1a80e6d4db86a27",
+    "url": "/new-/static/media/arrow-right.768dd837.svg"
+  },
+  {
+    "revision": "7793f54f8c4d2be0d858ebd15bee5ce1",
+    "url": "/new-/static/media/bott1.7793f54f.png"
+  },
+  {
+    "revision": "53ae2667a30efa95a6473c7a20f5977f",
+    "url": "/new-/static/media/bott2.53ae2667.png"
+  },
+  {
+    "revision": "b3399c6bb9c267b09a2b0984ea3803b7",
+    "url": "/new-/static/media/cosmetic-bottle.b3399c6b.png"
+  },
+  {
+    "revision": "e0444a4b840e6d5e554074d89f1b95ee",
+    "url": "/new-/static/media/cosmetic-cream.e0444a4b.png"
+  },
+  {
+    "revision": "b3ce2b853737bd21e18a38d2d65508a6",
+    "url": "/new-/static/media/cosmetics-bottles.b3ce2b85.png"
+  },
+  {
+    "revision": "2fc63df518a0b16c16474088a756c916",
+    "url": "/new-/static/media/cosmetics-jar.2fc63df5.png"
+  },
+  {
+    "revision": "4ab3b82d6aaab85de334082a8aca4061",
+    "url": "/new-/static/media/cosmetics-lady.4ab3b82d.png"
+  },
+  {
+    "revision": "6362df8b2e0007d90c72bb44ea29b59c",
+    "url": "/new-/static/media/cosmetics-tools.6362df8b.png"
+  },
+  {
+    "revision": "424efaa7afd6655b5b8fdf0406e98e1a",
+    "url": "/new-/static/media/cosmetics-two-bottles.424efaa7.png"
+  },
+  {
+    "revision": "1ee409965797ddfc02c8cac775f0da28",
+    "url": "/new-/static/media/cosmetics-yellow-jar.1ee40996.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/new-/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/new-/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/new-/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/new-/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b45062a9fbef1819bd6b60d525c23e76",
+    "url": "/new-/static/media/user.b45062a9.svg"
+  }
+]);
